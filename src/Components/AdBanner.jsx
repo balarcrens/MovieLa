ecbf6386c9d5b9073be2a1d911c072c8e86aac4d
balarcrens <balarcrens@gmail.com>
2025-09-29 16:46:23 +0530

    change adsense component

--- src/Components/AdBanner.jsx
+++ src/Components/AdBanner.jsx
@@ -1,24 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 function AdBanner() {
-    const adSlot = "2024123254";
+    const adRef = useRef(null);
 
     useEffect(() => {
-        if (window.adsbygoogle) {
-            try {
+        try {
+            if (window.adsbygoogle && adRef.current) {
                 (window.adsbygoogle = window.adsbygoogle || []).push({});
-            } catch (e) {
-                console.error("AdSense error:", e);
             }
+        } catch (e) {
+            console.error("AdSense error:", e);
         }
     }, []);
 
     return (
         <ins
+            ref={adRef}
             className="adsbygoogle"
             style={{ display: "block" }}
             data-ad-client="ca-pub-2791325015173516"
-            data-ad-slot={adSlot}
+            data-ad-slot="2024123254"
             data-ad-format="auto"
             data-full-width-responsive="true"
         ></ins>
