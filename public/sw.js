b774d122dc0293f7a0fbd42a515144e186edadf0
balarcrens <balarcrens@gmail.com>
2026-03-13 14:32:21 +0530

    add tag & sw.js file

---
+++ public/sw.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10723959
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
